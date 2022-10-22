09a60b1e9d78b04c05a8f4ca217ec3a4ade40cdd
Pascual Hdez <pascual.hernandez@wizeline.com>
2022-10-22 08:55:18 -0500

    enhance proxy to be able to handle all API calls

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-var express = require("express");
-var request = require("request");
-var cors = require("cors");
-var app = express();
+const express = require("express");
+const request = require("request");
+const cors = require("cors");
+const app = express();
 require("dotenv").config();
-
-var map = require("./proxyRouter");
 require("@babel/polyfill");
+const map = require("./proxyRouter");
+const authorization = require("./authorization");
 
 const getMap = (url) => {
-  let proxyMap = map;
-  let urlParts = url.split("/");
+  const proxyMap = map;
+  const urlParts = url.split("/");
   let foundMap = null;
   for (let i = 0; i <= urlParts.length; i++) {
     if (!foundMap) {
@@ -26,16 +26,31 @@ const getMap = (url) => {
   return foundMap;
 };
 
+const addApiCodeParameter = (path, code) => {
+  try {
+    if (path.includes("?")) {
+      return `${path}&code=${code}`;
+    }
+    return `${path}?code=${code}`;
+  } catch (err) {
+    console.log(err);
+    return path;
+  }
+};
+
 const rewriteURL = (protocol, host, url) => {
-  let completeUrl = `${protocol}://${host}${url}`;
-  let path = getMap(url);
-  if (path) {
-    let regex = new RegExp(Object.keys(path.pathRewrite)[0]);
-    var replaced = completeUrl.replace(
+  const completeUrl = `${protocol}://${host}${url}`;
+  const urlMap = getMap(url);
+  if (urlMap) {
+    const regex = new RegExp(Object.keys(urlMap.pathRewrite)[0]);
+    let replaced = completeUrl.replace(
       regex,
-      path.pathRewrite[Object.keys(path.pathRewrite)[0]]
+      urlMap.pathRewrite[Object.keys(urlMap.pathRewrite)[0]]
     );
-    return `${path.target}${replaced}`;
+    if (urlMap.code) {
+      replaced = addApiCodeParameter(replaced, urlMap.code);
+    }
+    return `${urlMap.target}${replaced}`;
   } else return completeUrl;
 };
 
@@ -45,42 +60,46 @@ app.use("/testConfig", async function (req, res) {
   let secApiCode = process.env.MOBILE_SECURITY_API_CODE;
   res.json({
     test: "hello",
-    //secApi,
-    //secApiCode,
   });
 });
 
 app.use("/", async function (req, res) {
-  //var url = apiServerHost + req.url;
   try {
-    var url = rewriteURL(req.protocol, req.get("Host"), req.url);
-    var urlParts = req.url.split("/");
-
-    console.log("url to do de request: ", url);
-    req
-      .pipe(
-        request({ url: url }, (error, response, body) => {
-          if (error) {
-            console.log(
-              `An error occurs in the following url: ${url}: `,
-              error
-            );
-            let message = "dev proxy error: ";
-            if (error.code === "ECONNREFUSED") {
-              message = message.concat("Refused connection");
-            } else if (error.code === "ECONNRESET") {
-              message = message.concat("The target connection has been lost");
-            } else {
-              message = message.concat("Unhandled error");
+    const url = rewriteURL(req.protocol, req.get("Host"), req.url);
+    const authToken = req.headers["authorization"];
+    const validToken = await authorization.validateAuthorization(
+      authToken,
+      req.url
+    );
+    if (validToken) {
+      console.log("request redirected to: ", url);
+      req
+        .pipe(
+          request({ url: url }, (error, response, body) => {
+            if (error) {
+              console.log(
+                `An error occurs in the following url: ${url}: `,
+                error
+              );
+              let message = "dev proxy error: ";
+              if (error.code === "ECONNREFUSED") {
+                message = message.concat("Refused connection");
+              } else if (error.code === "ECONNRESET") {
+                message = message.concat("The target connection has been lost");
+              } else {
+                message = message.concat("Unhandled error");
+              }
+              req.res.status(500).json({
+                errorMessage: message || "",
+                exception: message || {},
+              });
             }
-            req.res.status(500).json({
-              errorMessage: message || "",
-              exception: message || {},
-            });
-          }
-        })
-      )
-      .pipe(res);
+          })
+        )
+        .pipe(res);
+    } else {
+      req.res.status(401).json({ message: "Unauthorized" });
+    }
   } catch (ex) {
     req.res.status(500).json({
       exception: ex,
--- src/proxyRouter.js
+++ src/proxyRouter.js
@@ -7,24 +7,28 @@ const map = {
   },
   "/apiServices": {
     target: `${process.env.SERVICES_API}`,
+    code: process.env.SERVICES_API_CODE,
     pathRewrite: {
       "^.+apiServices": "",
     },
   },
   "/apiPayments": {
     target: `${process.env.PAYMENTS_API}`,
+    code: process.env.PAYMENTS_API_CODE,
     pathRewrite: {
       "^.+apiPayments": "",
     },
   },
   "/apiSurveys": {
     target: `${process.env.SURVEYS_API}`,
+    code: process.env.SURVEYS_API_CODE,
     pathRewrite: {
       "^.+apiSurveys": "",
     },
   },
   "/apiSpaces": {
     target: `${process.env.SPACES_API}`,
+    code: process.env.SPACES_API_CODE,
     pathRewrite: {
       "^.+apiSpaces": "",
     },
